e6bd4fbeee24228bd59c68be036d7c9ed6c5a7f4
Hendursaga <65990626+hendursaga@users.noreply.github.com>
2024-07-21 20:37:20 -0400

    Reformat src/package.lisp

--- src/package.lisp
+++ src/package.lisp
@@ -3,82 +3,82 @@
 (defpackage webdriver-client
   (:use :cl :assoc-utils)
   (:nicknames :webdriver)
-  (:export :*default-capabilities*
-           :make-capabilities
-           :merge-capabilities
-	   :chrome-capabilities
-           :firefox-capabilities
+  (:export #:*default-capabilities*
+           #:make-capabilities
+           #:merge-capabilities
+           #:chrome-capabilities
+           #:firefox-capabilities
 
-           :webdriver-status
+           #:webdriver-status
 
-           :make-session
-           :delete-session
-           :with-session
-           :use-session
+           #:make-session
+           #:delete-session
+           #:with-session
+           #:use-session
 
-           :start-interactive-session
-           :stop-interactive-session
+           #:start-interactive-session
+           #:stop-interactive-session
 
-           :key
-	   :keys
+           #:key
+           #:keys
 
-           :url
-           :back
-           :refresh
-           :page-title
-           :page-source
+           #:url
+           #:back
+           #:refresh
+           #:page-title
+           #:page-source
 
-           :find-element
-           :find-elements
-           :active-element
-           :element-clear
-           :element-click
-           :element-displayed
-           :element-enabled
-           :element-rect
-           :element-send-keys
-           :element-id
-           :element-text
-           :element-tagname
-           :element-attribute
+           #:find-element
+           #:find-elements
+           #:active-element
+           #:element-clear
+           #:element-click
+           #:element-displayed
+           #:element-enabled
+           #:element-rect
+           #:element-send-keys
+           #:element-id
+           #:element-text
+           #:element-tagname
+           #:element-attribute
 
-	   ;; contexts
-	   :get-window-handle
-	   :get-window-handles
-           :close-window
-           :switch-to-window
-           :switch-to-frame
-           :new-window
+           ;; contexts
+           #:get-window-handle
+           #:get-window-handles
+           #:close-window
+           #:switch-to-window
+           #:switch-to-frame
+           #:new-window
 
-           :mouse-move-to
-           :mouse-click
+           #:mouse-move-to
+           #:mouse-click
 
-           :make-cookie
-           :cookie
-           :delete-cookie
-           :delete-all-cookies
-           :find-cookie
+           #:make-cookie
+           #:cookie
+           #:delete-cookie
+           #:delete-all-cookies
+           #:find-cookie
 
-           :log-types
-           :logs
+           #:log-types
+           #:logs
 
-           :screenshot
-           :element-screenshot
+           #:screenshot
+           #:element-screenshot
 
-           :dismiss-alert
-           :accept-alert
-           :alert-text
+           #:dismiss-alert
+           #:accept-alert
+           #:alert-text
 
-	   :perform-actions
+           #:perform-actions
 
-           :execute-script
+           #:execute-script
 
-           :no-such-element-error
+           #:no-such-element-error
 
-           :window-resize)
-  (:import-from :alexandria
-                :with-gensyms
-                :assoc-value)
+           #:window-resize)
+  (:import-from  #:alexandria
+                 #:with-gensyms
+                 #:assoc-value)
   (:documentation "This package exports functions for working with Selenium WebDriver.
 
 For documentation see:
@@ -87,23 +87,23 @@ For documentation see:
 
 (defpackage webdriver-client-utils
   (:use :cl :webdriver-client)
-  (:export :*timeout*
-           :*default-element-func*
-           :find-elem
-           :wait-for
-           :get-cookie
-           :elem
-           :attr
-           :id
-           :classname
-           :classlist
-           :text
-           :send-key
-           :send-keys
-           :click)
-  (:import-from :alexandria
-                :assoc-value
-                :rcurry)
+  (:export #:*timeout*
+           #:*default-element-func*
+           #:find-elem
+           #:wait-for
+           #:get-cookie
+           #:elem
+           #:attr
+           #:id
+           #:classname
+           #:classlist
+           #:text
+           #:send-key
+           #:send-keys
+           #:click)
+  (:import-from #:alexandria
+                #:assoc-value
+                #:rcurry)
   (:documentation "Package with the purpose of reducing boilerplate.
 
 The exported definitions work with an implicit element. The default implicit element is the current active element. So, it is not neccesary to pass the element you are working with around most of the time.
@@ -112,9 +112,8 @@ The exported definitions work with an implicit element. The default implicit ele
 (defpackage webdriver-client-user
   (:nicknames :webdriver-user)
   (:use :cl :webdriver-client)
-  (:import-from
-   :webdriver-client-utils
-   :send-keys
-   :click
-   :wait-for)
+  (:import-from #:webdriver-client-utils
+                #:send-keys
+                #:click
+                #:wait-for)
   (:documentation "User package for interacting with WebDriver (interactive session)."))
